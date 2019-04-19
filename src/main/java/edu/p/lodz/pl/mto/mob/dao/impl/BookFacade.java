538a152fd408d4ab2f3e0967566e75db894acdd9
180597 Kozłowicz Tomasz <180597@edu.p.lodz.pl>
2019-04-19 08:13:10 +0200

    Entities with facades + custom filter for Book - Rental relation

---
+++ src/main/java/edu/p/lodz/pl/mto/mob/dao/impl/BookFacade.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.p.lodz.pl.mto.mob.dao.impl;
+
+import edu.p.lodz.pl.mto.entities.Book;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.TransactionRolledbackException;
+import edu.p.lodz.pl.mto.mob.dao.BookFacadeLocal;
+
+/**
+ *
+ * @author Tomasz
+ */
+@Stateless
+@TransactionAttribute(TransactionAttributeType.MANDATORY)
+public class BookFacade implements BookFacadeLocal{
+
+    @PersistenceContext(unitName = "mtomob_pu")
+    private EntityManager em;
+    
+     protected static final Logger loger = Logger.getGlobal();
+
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    @Override
+    public List<Book> getAllBooks() throws TransactionRolledbackException {
+        TypedQuery<Book> tq = em.createNamedQuery("Book.findAll", Book.class);
+        return tq.getResultList();
+    }
+    @Override
+     public Book find(Object id) {
+        return getEntityManager().find(Book.class, id);
+    }
+    
+}
