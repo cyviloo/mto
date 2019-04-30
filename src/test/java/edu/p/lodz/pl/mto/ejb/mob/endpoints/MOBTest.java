13747a1c7a3172aa60c596b0be5070b57a1308b5
Borys Cywinski <204192@edu.p.lodz.pl>
2019-04-30 10:56:28 +0200

    Revert "Revert "Dummy test of an EJB""
    This reverts commit 3a26b7415b082cca8d8dd95769e34728ba0f3a21.

--- src/test/java/edu/p/lodz/pl/mto/ejb/mob/endpoints/MOBTest.java
+++ src/test/java/edu/p/lodz/pl/mto/ejb/mob/endpoints/MOBTest.java
@@ -5,12 +5,18 @@
  */
 package edu.p.lodz.pl.mto.ejb.mob.endpoints;
 
+import edu.p.lodz.pl.mto.ejb.mob.endpoints.impl.MOBEndpoint;
+import javax.ejb.EJB;
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.validation.constraints.AssertTrue;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -18,15 +24,26 @@ import static org.junit.Assert.*;
  */
 public class MOBTest {
     
+    @EJB
+    private static MOBEndpoint endpoint;
+    private static EJBContainer ejbContainer;
+    private static Context ctx;
+    
     public MOBTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        ejbContainer = EJBContainer.createEJBContainer();
+        //System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+        //System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");  
+        
+        ctx = ejbContainer.getContext();
     }
     
     @AfterClass
     public static void tearDownClass() {
+        ejbContainer.close();
     }
     
     @Before
@@ -38,7 +55,10 @@ public class MOBTest {
     }
     
     @Test
-    public void test1() { assertFalse(false); }
+    public void test1() throws NamingException {
+        endpoint = (MOBEndpoint)ctx.lookup("java:global/classes/MOBEndpoint");
+        Assert.assertEquals(endpoint.getAllBooks().size(), 20);
+    }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
