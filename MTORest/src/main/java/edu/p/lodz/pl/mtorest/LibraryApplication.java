18973d03e7ab8a72d701da3a172098f9e56781a1
tomasz <180597@edu.p.lodz.pl>
2019-05-11 06:46:39 +0200

    Main path

---
+++ MTORest/src/main/java/edu/p/lodz/pl/mtorest/LibraryApplication.java
@@ -0,0 +1,10 @@
+package edu.p.lodz.pl.mtorest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/")
+public class LibraryApplication extends Application {
+
+}
